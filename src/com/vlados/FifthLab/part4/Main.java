15b477cd31488a58cf80947dacd7d95f0fc3a054
vladachugay <chugay.vlada@gmail.com>
2020-11-01 13:54:15 +0200

    Lab5 part4. Implemented method for getting HTML file with URL in IOService.

---
+++ src/com/vlados/FifthLab/part4/Main.java
@@ -0,0 +1,14 @@
+package com.vlados.FifthLab.part4;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+//        try {
+//            System.out.println(new IOService().getHTMLFile(new URL("https://www.codeflow.site/ru/article/java-url")));
+//        } catch (IOException exception) {
+//            System.out.println(exception.getMessage());
+//        }
+    }
+}
