3435c38077ff0c7fa4ee11a918d1a9548a030d31
vladachugay <chugay.vlada@gmail.com>
2020-11-01 16:33:26 +0200

    Added users menu. Lab5 is completed.

--- src/com/vlados/FifthLab/part4/Controller.java
+++ src/com/vlados/FifthLab/part4/Controller.java
@@ -1,14 +1,62 @@
 package com.vlados.FifthLab.part4;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Controller {
 
+    public void run() {
+        Map<String, Integer> map;
+        while(true) {
+            try {
+                map = this.countTags(new IOService().getHTMLFile(this.retrieveURL()));
+                System.out.println("result: \n" + map);
+                break;
+            } catch (IOException exception) {
+                System.out.println("Some exception occurred. " + exception.toString());
+                System.out.println("Try again.");
+            }
+        }
+        while (true) {
+            System.out.println("\n1. Sort by quantity.\n" +
+                    "2. Sort by name\n" +
+                    "3. Exit");
+            switch (this.retrieveUsersLine().trim()) {
+                case "1" -> this.sortByQuantity(map);
+                case "2" -> this.sortByName(map);
+                case "3" -> System.exit(1);
+                default -> System.out.println("Incorrect input.\n");
+            }
+        }
+    }
+
+    public String retrieveUsersLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().trim();
+    }
+
+    public URL retrieveURL() {
+        URL url;
+        while(true) {
+            try {
+                System.out.println("Enter URL:");
+                url = new URL(this.retrieveUsersLine());
+                break;
+            } catch (MalformedURLException e) {
+                System.out.println("Incorrect input: " + e.getMessage());
+                System.out.println("Try again.");
+            }
+        }
+        return url;
+    }
+
     public Map<String, Integer> countTags(String text) {
-        String regex = "<[a-z]{1,}[\\s>]";
+        String regex = "<[a-z]+[\\s>]";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
         Map<String, Integer> map = new HashMap<>();
@@ -21,5 +69,19 @@ public class Controller {
         return map;
     }
 
-    
+    public void sortByName(Map<String, Integer> unsortedMap) {
+        Map<String, Integer> result = unsortedMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        System.out.println(result);
+    }
+
+    public void sortByQuantity(Map<String, Integer> unsortedMap) {
+        Map<String, Integer> result = unsortedMap.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        System.out.println(result);
+    }
 }
--- src/com/vlados/FifthLab/part4/Main.java
+++ src/com/vlados/FifthLab/part4/Main.java
@@ -1,15 +1,21 @@
 package com.vlados.FifthLab.part4;
 
-import java.io.IOException;
-import java.net.URL;
-
 public class Main {
     public static void main(String[] args) {
-        try {
-            URL url = new URL("https://github.com/vladachugay");
-            System.out.println(new Controller().countTags(new IOService().getHTMLFile(url)));
-        } catch (IOException exception) {
-            System.out.println(exception.getMessage());
-        }
+        new Controller().run();
+//        Controller controller = new Controller();
+//        Map<String, Integer> map = new HashMap<>();
+//        try {
+//            URL url = new URL("https://github.com/vladachugay");
+//            map = controller.countTags(new IOService().getHTMLFile(url));
+//            System.out.println(map);
+//            System.out.println();
+//            controller.sortByName(map);
+//            System.out.println();
+//            controller.sortByQuantity(map);
+//
+//        } catch (IOException exception) {
+//            System.out.println(exception.getMessage());
+//        }
     }
 }
