4b5cd38c19deabb088810975fc5d0ab9fec09894
vladachugay <chugay.vlada@gmail.com>
2020-11-04 22:40:40 +0200

    completed task1_2 from Lab7 hard level.

--- src/com/vlados/lab7hard/main/task_1_2/Factorial.java
+++ src/com/vlados/lab7hard/main/task_1_2/Factorial.java
@@ -1,6 +1,7 @@
 package com.vlados.lab7hard.main.task_1_2;
 
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 /**
  * Created by olenasyrota on 6/28/16.
@@ -9,8 +10,7 @@ public class Factorial {
 
 
     public static long factorial(long i) {
-        // implement function body in order all test to complete successfully
-        return 1;
+        return Stream.iterate(1, n -> n + 1).limit(i).reduce((x, y) -> x * y).orElseThrow();
 
     }
 }
